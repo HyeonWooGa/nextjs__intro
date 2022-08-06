ab7c1acfcd352998166fd20399c1aaf9fbaa5772
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-06 23:06:34 +0900

    img 추가, Fregment 추가

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,36 +5,45 @@ export default function NavBar() {
   const router = useRouter();
   // console.log(router);
   return (
-    <nav>
-      <div>
-        <Link href="/">
-          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
-        </Link>
-        <Link href="/about">
-          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
-        </Link>
-      </div>
-      <style jsx>{`
-        nav {
-          display: flex;
-          gap: 10px;
-          flex-direction: column;
-          align-items: center;
-          padding-top: 20px;
-          padding-bottom: 10px;
-        }
-        nav a {
-          font-weight: 600;
-          font-size: 18px;
-        }
-        .active {
-          color: tomato;
-        }
-        nav div {
-          display: flex;
-          gap: 10px;
-        }
-      `}</style>
-    </nav>
+    <>
+      <nav>
+        <img src="/hyeonwooga.png" />
+        <div>
+          <Link href="/">
+            <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+          </Link>
+          <Link href="/about">
+            <a className={router.pathname === "/about" ? "active" : ""}>
+              About
+            </a>
+          </Link>
+        </div>
+        <style jsx>{`
+          nav {
+            display: flex;
+            gap: 10px;
+            flex-direction: column;
+            align-items: center;
+            padding-top: 20px;
+            padding-bottom: 10px;
+          }
+          img {
+            max-width: 100px;
+            margin-bottom: 5px;
+          }
+          nav a {
+            font-weight: 600;
+            font-size: 18px;
+          }
+          .active {
+            color: tomato;
+          }
+          nav div {
+            display: flex;
+            gap: 10px;
+          }
+        `}</style>
+      </nav>
+    </>
   );
 }
