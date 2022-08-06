938913056e4ffd80c7d9f0da0db0052e37b9e225
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-07 00:06:25 +0900

    Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "b9a221486250d0601edc387fbf688741";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      // console.log(data);
+      // console.log(results);
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <>
       <Seo title="Home" />
-      <h1>Hello World!</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.title}</h4>
+        </div>
+      ))}
     </>
   );
 }
