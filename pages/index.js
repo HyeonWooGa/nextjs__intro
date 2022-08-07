03b9e7d1f340847b8f8a58faf5f3b61a03aafed7
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-07 16:33:26 +0900

    Redirects and Rewrites

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,11 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "b9a221486250d0601edc387fbf688741";
-
 export default function Home() {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       // console.log(data);
       console.log(results);
       setMovies(results);
