690b34229ddf265de4d584578f447334881f239e
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-07 14:33:24 +0900

    홈 페이지 스타일 수정

--- pages/index.js
+++ pages/index.js
@@ -13,20 +13,41 @@ export default function Home() {
         )
       ).json();
       // console.log(data);
-      // console.log(results);
+      console.log(results);
       setMovies(results);
     })();
   }, []);
 
   return (
-    <>
+    <div className="container">
       <Seo title="Home" />
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie) => (
-        <div key={movie.id}>
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.title}</h4>
         </div>
       ))}
-    </>
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
   );
 }
