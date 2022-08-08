2f9c934b24e0766586320b0160852140370ccee9
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-09 00:42:21 +0900

    Router Hook

--- next.config.js
+++ next.config.js
@@ -14,14 +14,18 @@ const nextConfig = {
       },
     ];
   },
-  /*async rewrites() {
+  async rewrites() {
     return [
-      {
+      /*{
         source: "/api/movies",
         destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },*/
+      {
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
       },
     ];
-  },*/
+  },
 };
 
 module.exports = nextConfig;
